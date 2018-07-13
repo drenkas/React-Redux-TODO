2eb8a90ba3d5d87e314e11e4cb0764bba8d86b13
drenkas <33375208+drenkas@users.noreply.github.com>
2018-07-13 13:28:40 +0300

    delete comment

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -4,63 +4,9 @@ import Button from 'react-bootstrap-button-loader';
 import ModalInput from './Modal'
 
 class Post extends Component {
-	/* constructor() {
-		super(...arguments);
-
-		this.state = {editing: false};
-	}
-
-	edit = () => {
-		this.setState({editing: true});
-	}
-
-	stopEdit = () => {
-		this.setState({editing: false});
-	} */
-
 	delete = () => {
 		this.props.postDelete(this.props.id);
 	}
-
-	/* renderModalInput(text) {
-		let input;
-
-		return (
-			<div className="static-modal">
-				<Modal.Dialog>
-					<Modal.Header>
-						<Modal.Title>Modal title</Modal.Title>
-					</Modal.Header>
-					<Modal.Body>
-						<Form inline 
-						className="justify-content-between"
-						onSubmit={e => {
-							e.preventDefault()
-							if (!input.value.trim()) {
-								return
-							}
-							this.stopEdit();
-							this.props.postEdit(input.value, this.props.id);
-							input.value = ''
-						}}>
-							<FormGroup controlId="formEditTodo">
-								<FormControl defaultValue={text}
-									maxLength="20"
-									type="text"
-									inputRef={node => {
-									input = node
-								}}/>
-							</FormGroup>
-							<Button bsStyle="success" type="submit">Done</Button>
-						</Form>
-					</Modal.Body>
-					<Modal.Footer>
-						<Button bsStyle="danger" onClick={this.stopEdit}>Close</Button>
-					</Modal.Footer>
-				</Modal.Dialog>
-			</div>
-		);
-	  } */
 
 	renderLable(text) {
 		return (
